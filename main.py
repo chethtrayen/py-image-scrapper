9436878c619126bd89f6cd601c9d21784e954710
chethtrayen <chethtrayen1991@gmail.com>
2023-03-12 16:36:30 -0400

    Add comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.scrap import scrapImages
 from utils.image import getImageSources
 
+# Update to site to scrap
 URL = "https://kirbyscomicartshop.com/collections/jon-lam"
 images = scrapImages(URL)
 sources = getImageSources(images)
